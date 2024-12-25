f1def05ba943ff76e668aae2b545c1f8fe1ff64b
Phong Le <phong_le@epam.com>
2024-12-25 08:07:38 +0700

    Add place holder for application.properties

--- src/main/java/com/learn/oct2024/betting_service/configuration/WebConfig.java
+++ src/main/java/com/learn/oct2024/betting_service/configuration/WebConfig.java
@@ -2,6 +2,7 @@ package com.learn.oct2024.betting_service.configuration;
 
 import com.learn.oct2024.betting_service.interceptor.TokenValidationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,10 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private TokenValidationInterceptor tokenValidationInterceptor;
 
+    @Value("${react.client.domain}")
+    private String REACT_CLIENT;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(REACT_CLIENT)
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "Cache-Control")
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
